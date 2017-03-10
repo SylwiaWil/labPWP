28ca1de9c90f3549d10c18d6de7eb933c88c5366
Student <student@ib.polsl.pl>
2017-03-10 13:31:11 +0100

    use trigonomery library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include "trygonometria.h"
+#include<LaborkaConfig.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include<trygonometria.h>
+#else
+	#define _USE_MATH_DEFINES
+	#include<math.h>
+#endif
 
  int main()
  {
+	 double result; 
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		result = cos_degree(45.0);
+	#else
+		result = cos(pi/4.0);
+	#endif
+
 	double sin_degree(10); 
 
 return 0;
